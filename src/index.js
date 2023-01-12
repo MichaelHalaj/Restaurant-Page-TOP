5941368ca476cfc22308cb66f7f69c8e6e1057b4
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-12 15:15:05 -0500

    Add menu interaction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import './style.css';
 import { generate } from './home';
+import createMenu from './menu';
 
+generate();
 const homeTab = document.createElement('div');
+const menuTab = document.createElement('div');
+
 homeTab.textContent = 'Home';
+menuTab.textContent = 'Menu';
 document.body.appendChild(homeTab);
+document.body.appendChild(menuTab);
 homeTab.addEventListener('click', generate);
+menuTab.addEventListener('click', createMenu);
---
+++ src/menu.js
@@ -0,0 +1,28 @@
+const menuItem = (name, id, price, description) => ({ name, id, price, description });
+
+const createMenu = () => {
+    const content = document.getElementById('content');
+    let menu = [];
+    let itemId = 0;
+    menu.push(menuItem('Vanilla', itemId++, 0.99, 'Standard'));
+    menu.push(menuItem('Chocolate', itemId++, 0.99, 'Standard'));
+
+    for(let i = 0; i < menu.length; i++){
+        const item = document.createElement('div');
+        item.classList.add('menu-item');
+        const name = document.createElement('h3');
+        const price = document.createElement('div');
+        const description = document.createElement('div');
+
+        name.textContent = menu[i].name;
+        price.textContent = `${menu[i].price}`;
+        description.textContent = menu[i].description;
+        item.appendChild(name);
+        item.appendChild(price);
+        item.appendChild(description);
+        content.appendChild(item);
+    }
+
+};
+
+export default createMenu;
