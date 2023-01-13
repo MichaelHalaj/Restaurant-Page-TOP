232ff9d5170a889d5c5bf4ee82fb23a7a01a1759
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-13 12:11:07 -0500

    Add images to menu

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,32 @@
 import { removeAllChildren } from "./functions";
-const menuItem = (name, id, price, description) => ({ name, id, price, description });
+import vanilla from './dovile-ramoskaite-vanilla.jpg';
+import chocolate from './mae-mu-chocolate.jpg';
+const menuItem = (name, id, price, description, image ) => ({ name, id, price, description, image });
 
 const createMenu = (content) => {
     removeAllChildren(content);
     let menu = [];
     let itemId = 0;
-    menu.push(menuItem('Vanilla', itemId++, 0.99, 'Standard'));
-    menu.push(menuItem('Chocolate', itemId++, 0.99, 'Standard'));
+    menu.push(menuItem('Vanilla', itemId++, 0.99, 'Standard', vanilla));
+    menu.push(menuItem('Chocolate', itemId++, 0.99, 'Standard', chocolate));
 
     for(let i = 0; i < menu.length; i++){
         const item = document.createElement('div');
         item.classList.add('menu-item');
+        const element = document.createElement('div');
+        element.classList.add(menu[i].name);
         const name = document.createElement('h3');
         const price = document.createElement('div');
         const description = document.createElement('div');
-
+        const image = new Image();
+        image.src = menu[i].image;
+        image.setAttribute('width', '360px');
+        image.setAttribute('height', '540px');
+        element.appendChild(image);
         name.textContent = menu[i].name;
         price.textContent = `$${menu[i].price}`;
         description.textContent = menu[i].description;
+        item.appendChild(element);
         item.appendChild(name);
         item.appendChild(price);
         item.appendChild(description);
